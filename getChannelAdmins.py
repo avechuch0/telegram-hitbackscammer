92c27c21d6bd1aa3b3bd610673de1e6de3e027f3
Jaime Andrés Bello Vieda <121986639+avechuch0@users.noreply.github.com>
2023-02-20 21:40:14 -0500

    Update getChannelAdmins.py

--- getChannelAdmins.py
+++ getChannelAdmins.py
@@ -18,6 +18,7 @@ bot_username = config['Telegram']['bot_username']
 init(autoreset=False)
 
 # Creating the 'bot' session
+print(Fore.GREEN + "[+] We are creating the session file for bot, input the bot token (i.e. 4509046619:LMnjdork...)" + Style.RESET_ALL)
 bot = TelegramClient(bot_username, api_id, api_hash)
 
 async def main():
