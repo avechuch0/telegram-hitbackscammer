de8457624f7b06c6567b5cf32e43a0a06af4a83a
Jaime Andrés Bello Vieda <121986639+avechuch0@users.noreply.github.com>
2023-02-20 21:46:08 -0500

    Update deletePrivateChannelMsgs.py

--- deletePrivateChannelMsgs.py
+++ deletePrivateChannelMsgs.py
@@ -17,8 +17,8 @@ bot_username = config['Telegram']['bot_username']
 init(autoreset=False)
 
 # Creating the bot session
+print(Fore.GREEN + "[+] We are creating the session file for bot, input the bot token (i.e. 4509046619:LMnjdork...)" + Style.RESET_ALL)
 bot = TelegramClient(bot_username, api_id, api_hash)
-print("Creating the bot user client")
 bot.start()
 
 async def get_scammer_channel():
