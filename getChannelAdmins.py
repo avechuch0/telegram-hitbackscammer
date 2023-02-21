922924ce46adb87fbb0b38a9828b35b92ee5eb48
Jaime Andrés Bello Vieda <121986639+avechuch0@users.noreply.github.com>
2023-02-20 23:17:19 -0500

    Update getChannelAdmins.py

--- getChannelAdmins.py
+++ getChannelAdmins.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import configparser
 import sys
+import os
 from telethon import TelegramClient, types
 from tabulate import tabulate
 from colorama import Fore, Style, init
@@ -18,7 +19,8 @@ bot_username = config['Telegram']['bot_username']
 init(autoreset=False)
 
 # Creating the 'bot' session
-print(Fore.GREEN + "[+] We are creating the session file for bot, input the bot token (i.e. 4509046619:LMnjdork...)" + Style.RESET_ALL)
+if not os.path.isfile(str(bot_username) + ".session"):
+    print(Fore.GREEN + "[+] We are creating the session file for bot, input the bot token (i.e. 4509046619:LMnjdork...)" + Style.RESET_ALL)
 bot = TelegramClient(bot_username, api_id, api_hash)
 
 async def main():
