7678f69a5f8f85ebf1484429f1e53464a47a3f1e
Jaime Andrés Bello Vieda <121986639+avechuch0@users.noreply.github.com>
2023-02-20 23:47:26 -0500

    Update telegram-hitbackscammer.py

--- telegram-hitbackscammer.py
+++ telegram-hitbackscammer.py
@@ -113,14 +113,12 @@ def check_config():
             # Creating the 'user' session
             if not os.path.isfile(str(user_username) + ".session"):
                 print(Fore.GREEN + "[+] We are creating the session file for user, input your mobile phone number to login (i.e. +57300...)" + Style.RESET_ALL)                    
-            # Creating the 'user' session
-            if not os.path.isfile(str(bot_username) + ".session"):
-                print(Fore.GREEN + "[+] We are creating the session file for user, input your mobile phone number to login (i.e. +57300...)" + Style.RESET_ALL)
             client = TelegramClient(user_username, api_id, api_hash)
             client.start()
             client.disconnect()
             # Creating the 'bot' session
-            print(Fore.GREEN + "[+] We are creating the session file for bot, input the bot token (i.e. 4509046619:LMnjdork...)" + Style.RESET_ALL)
+            if not os.path.isfile(str(bot_username) + ".session"):
+                print(Fore.GREEN + "[+] We are creating the session file for user, input your mobile phone number to login (i.e. +57300...)" + Style.RESET_ALL)
             bot = TelegramClient(bot_username, api_id, api_hash)
             bot.start()
             bot.disconnect()            
